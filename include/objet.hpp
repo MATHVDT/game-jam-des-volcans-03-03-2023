85d10cf8b140dd44e8bb44c4b8823e9ad42dc8bc
vameunier <valentin.meunier@etu.isima.fr>
2023-03-04 15:46:30 +0100

    plus de trucs de ermde

--- include/objet.hpp
+++ include/objet.hpp
@@ -1,19 +1,24 @@
 #ifndef __OBJET_HPP__
 #define __OBJET_HPP__
+#include <SFML/Graphics/RenderStates.hpp>
 #include <SFML/System/Vector2.hpp>
 #include <SFML/Graphics/Rect.hpp>
 #include <SFML/Graphics/Sprite.hpp>
 #include "enum_type.hpp"
 #include <vector>
-
+#include <map>
+#include <string>
+#include <filesystem>
 class Objet
 {
 private:
     bool visible;
     sf::Rect<int> rectangle;
     sf::Sprite sprite;
+	//	static std::map<std::string, sf::Texture> texture;
 
 public:
+		static void initialisationTexture();
     virtual bool estIphone() = 0;
     virtual void clic() = 0;
     const sf::Rect<int> &obtenirRectangle() const;
--- src/objet.cpp
+++ src/objet.cpp
@@ -1,18 +1,30 @@
 #include "../include/objet.hpp"
 #include <SFML/Graphics/Rect.hpp>
+#include <SFML/Graphics/RenderStates.hpp>
 
-const sf::Rect<int>& Objet::obtenirRectangle() const{
+//std::map<std::string, sf::Texture> Objet::texture = {};
+
+const sf::Rect<int>& Objet::obtenirRectangle() const
+{
 				return rectangle;
 }
 
-
-void Objet::definirRectangle(int gauche, int haut) {
+void Objet::definirRectangle(int gauche, int haut)
+{
 				rectangle.left = gauche;
 				rectangle.top = haut;
 }
 
-void Objet::definirRectangle(int gauche, int haut, 	int largeur , int hauteur) {
-				definirRectangle(gauche,haut);
+void Objet::definirRectangle(int gauche, int haut, int largeur, int hauteur)
+{
+				definirRectangle(gauche, haut);
 				rectangle.width = largeur;
 				rectangle.height = hauteur;
 }
+
+/*void static Objet::initialisationTexture()
+{
+				const std::filesystem::path chemin { "../ressources/" };
+				for (auto& file : std::filesystem::directory_iterator { chemin }) // loop through the current folder
+				{ }
+}*/
